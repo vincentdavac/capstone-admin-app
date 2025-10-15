9e6308b860507bd8fefd5651401fa60a209df2ed
akosimario <166306717+akosimario@users.noreply.github.com>
2025-10-15 11:32:23 +0800

    deployed buoy cards

--- src/pages/Dashboard/deployedBuoy.tsx
+++ src/pages/Dashboard/deployedBuoy.tsx
@@ -2,12 +2,14 @@ import TropicalCard from "../../components/deployedBuoy/tropicalTable";
 import HistoricalCard from "../../components/deployedBuoy/historicalCards";
 import SearchBuoy from "../../components/deployedBuoy/search";
 import MapsWithHazard from "../../components/deployedBuoy/maps_w_buoyControl";
+import BuoyCondition from "../../components/deployedBuoy/buoyCondition";
 
 const tropicalPage = () => {
   return (
     <div className="">
       <SearchBuoy />
       <MapsWithHazard />
+      <BuoyCondition/>
       {/* <TropicalCard />
       <HistoricalCard /> */}
     </div>

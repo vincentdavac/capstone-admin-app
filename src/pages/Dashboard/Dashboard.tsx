f5d2b35843a56d2c5385a6134e163896a078c875
akosimario <166306717+akosimario@users.noreply.github.com>
2025-10-29 03:12:45 +0800

    auto alert in humidity

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -5,19 +5,29 @@ import MapDashboard from "../../components/dashboard_content/map";
 import ForecastCard from "../../components/dashboard_content/forecast";
 import { useEffect } from 'react';
 import  setAlert from "../../core_api_fetching/insertingAlert";
+import  setHumidityAlert  from "../../core_api_fetching/humidityAlert";
 
 export default function Dashboard() {
   useEffect(() => {
-    const sendAlert = async () => {
-      try {
-        await setAlert.post(); 
-      } catch (error) {
-        console.error('Failed to insert data:', error);
-      }
-    };
+  const sendAlert = async () => {
+    try {
+      await setAlert.post(); 
+    } catch (error) {
+      console.error('Failed to send alert:', error);
+    }
+  };
+  
+  const sendHumidityAlert = async () => {
+    try {
+      await setHumidityAlert.post(); 
+    } catch (error) {
+      console.error('Failed to set humidity alert:', error);
+    }
+  };
 
-    sendAlert();
-  }, []);
+  sendAlert();
+  sendHumidityAlert();
+}, []);
   return (
     <div>
       <DashboardCards />

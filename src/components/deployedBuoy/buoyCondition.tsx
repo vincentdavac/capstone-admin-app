7a3ac6b1ed016e7724f1884cf7464f367d92b47c
xxnieljohn <gonzaloseanruzze@gmail.com>
2025-11-22 00:49:38 +0800

    Deployed Buoy 1.2 card fix

--- src/components/deployedBuoy/buoyCondition.tsx
+++ src/components/deployedBuoy/buoyCondition.tsx
@@ -1,25 +1,37 @@
 export default function buoyCondition() {
   return (
-    <div className="mt-5 dark:bg-gray-900 p-4 rounded-lg"> 
+    <div className="mt-5 dark:bg-gray-900 p-4 rounded-lg">
       <div className="flex gap-6 flex-wrap justify-center lg:justify-start">
         <div className="bg-[#E2F1FF] dark:bg-gray-800 rounded-lg shadow-md p-6 flex flex-col justify-between h-[106px] w-full sm:w-80 md:w-[345px] overflow-hidden">
-          <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-100">GPS LOCATION</h3>
-          <p className="text-[18px] text-gray-600 dark:text-gray-400">120.95° E, 14.67° N</p>
+          <h3 className="text-lg font-semibold text-[#453EFE] dark:text-gray-100">
+            INITIAL DEPLOYMENT LOCATION
+          </h3>
+          <p className="text-[18px] text-[#FF0000] dark:text-gray-400">
+            Lat: 14.654321, Long: 120.987654
+          </p>
+        </div>
+
+        <div className="bg-[#E2F1FF] dark:bg-gray-800 rounded-lg shadow-md p-6 flex flex-col justify-between h-[106px] w-full sm:w-80 md:w-[345px] overflow-hidden">
+          <h3 className="text-lg font-semibold text-[#453EFE] dark:text-gray-100">
+            CURRENT GPS LOCATION{" "}
+          </h3>
+          <p className="text-[18px] text-[#FF0000]">
+            Lat: 14.654890, Long: 120.987900
+          </p>
+        </div>
+
+        <div className="bg-[#E2F1FF] dark:bg-gray-800 rounded-lg shadow-md p-6 flex flex-col justify-between h-[106px] w-full sm:w-80 md:w-[345px] overflow-hidden">
+          <h3 className="text-lg font-semibold text-[#453EFE] dark:text-gray-100">
+            BATTERY HEALTH
+          </h3>
+          <p className="text-[18px] text-[#5eff00bd]">85%</p>
         </div>
         
-        <div className="bg-[#E2F1FF] dark:bg-gray-800 rounded-lg shadow-md p-6 flex flex-col justify-between h-[106px] w-full sm:w-80 md:w-[345px] overflow-hidden">
-          <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-100">BATTERY HEALTH</h3>
-          <p className="text-[18px] text-[#53AE22]">85%</p>
-        </div>
-        
-        <div className="bg-[#E2F1FF] dark:bg-gray-800 rounded-lg shadow-md p-6 flex flex-col justify-between h-[106px] w-full sm:w-80 md:w-[345px] overflow-hidden">
-          <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-100">BUOY TEMPERATURE</h3>
-          <p className="text-[18px] text-[#FF0000]">29.3°C / 84.7°F</p>
-        </div>
-        
-        <div className="bg-[#E2F1FF] dark:bg-gray-800 rounded-lg shadow-md p-6 flex flex-col justify-between h-[106px] w-full sm:w-80 md:w-[345px] overflow-hidden">
-          <h3 className="text-lg font-semibold text-gray-800 dark:text-gray-100">HEIGHT IN SEA SURFACE</h3>
-          <p className="text-[18px] text-[#FFC107]">29.3°C</p>
+        <div className="bg-[#fcfcfcea] dark:bg-gray-800 rounded-lg shadow-md p-6 flex flex-col justify-between h-[106px] w-full sm:w-80 md:w-[345px] overflow-hidden">
+          <button className="flex items-center gap-3 px-6 py-3 border border-[#59FF00] rounded-[8px] bg-white hover:bg-gray-50 transition-colors dark:bg-gray-800 dark:hover:bg-gray-700 dark:border-green-400">
+              <span className="w-5 h-5 bg-[#59FF00] rounded-full"></span>
+              <span className="font-medium text-black dark:text-white">Lights On/Off</span>
+          </button>
         </div>
       </div>
     </div>
